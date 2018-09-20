07cf50f0370bda8decbca17ea6967c7acf8f0418
chidiyaa <42734701+chidiyaa@users.noreply.github.com>
2018-09-20 11:21:10 +0530

    Add files via upload

---
+++ MiniProject/src/com/cg/rms/beans/CandidatePersonal.java
@@ -0,0 +1,116 @@
+package com.cg.rms.beans;
+import java.sql.Blob;
+import java.util.Date;
+
+
+public class CandidatePersonal {
+
+    private String candidateId;
+    private String candidateName;
+    private String address;
+    private Date dob;
+    private String emailId;
+    private String contactNumber;
+    private String maritalStatus;
+    private String gender;
+    private String passportNumber;
+    private Blob candidatePhoto;
+    
+    public CandidatePersonal() {
+        super();
+        
+    }
+    
+    public CandidatePersonal(String candidateId, String candidateName,
+            String address, Date dob, String emailId, String contactNumber,
+            String maritalStatus, String gender, String passportNumber,
+            Blob candidatePhoto) {
+        super();
+        this.candidateId = candidateId;
+        this.candidateName = candidateName;
+        this.address = address;
+        this.dob = dob;
+        this.emailId = emailId;
+        this.contactNumber = contactNumber;
+        this.maritalStatus = maritalStatus;
+        this.gender = gender;
+        this.passportNumber = passportNumber;
+        this.candidatePhoto = candidatePhoto;
+    }
+
+    public String getCandidateId() {
+        return candidateId;
+    }
+    public void setCandidateId(String candidateId) {
+        this.candidateId = candidateId;
+    }
+    public String getCandidateName() {
+        return candidateName;
+    }
+    public void setCandidateName(String candidateName) {
+        this.candidateName = candidateName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public Date getDob() {
+        return dob;
+    }
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+    public String getEmailId() {
+        return emailId;
+    }
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+    public String getContactNumber() {
+        return contactNumber;
+    }
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+    public void setMaritalStatus(String maritalStatus) {
+        this.maritalStatus = maritalStatus;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public Blob getCandidatePhoto() {
+        return candidatePhoto;
+    }
+
+    public void setCandidatePhoto(Blob candidatePhoto) {
+        this.candidatePhoto = candidatePhoto;
+    }
+
+    @Override
+    public String toString() {
+        return "Candidate_Personal [candidateId=" + candidateId
+                + ", candidateName=" + candidateName + ", address=" + address
+                + ", dob=" + dob + ", emailId=" + emailId + ", contactNumber="
+                + contactNumber + ", maritalStatus=" + maritalStatus
+                + ", gender=" + gender + ", passportNumber=" + passportNumber
+                + ", candidatePhoto=" + candidatePhoto + "]";
+    }
+    
+    
+    
+}
